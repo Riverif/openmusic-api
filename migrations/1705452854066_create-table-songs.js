fab5bd8e889f373f35f1dbc706341b9d12c53bd0
Riverif <rifkialfiann@gmail.com>
2024-01-17 08:51:19 +0700

    albums done, songs progress

---
+++ migrations/1705452854066_create-table-songs.js
@@ -0,0 +1,38 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable("songs", {
+    id: {
+      type: "VARCHAR(50)",
+      primaryKey: true,
+    },
+    title: {
+      type: "text",
+      notNull: true,
+    },
+    year: {
+      type: "smallint",
+      notNull: true,
+    },
+    genre: {
+      type: "text",
+      notNull: true,
+    },
+    performer: {
+      type: "text",
+      notNull: true,
+    },
+    duration: {
+      type: "int",
+    },
+    albumId: {
+      type: "VARCHAR(50)",
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable("songs");
+};
