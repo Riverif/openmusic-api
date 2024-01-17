dcb5e5e041503a28ed5feb7a66dce396001a4302
Riverif <rifkialfiann@gmail.com>
2024-01-17 18:34:16 +0700

    albums and songs done

--- src/server.js
+++ src/server.js
@@ -2,12 +2,16 @@ require("dotenv").config();
 
 const Hapi = require("@hapi/hapi");
 const albums = require("./api/albums");
+const songs = require("./api/songs");
 const AlbumsService = require("./services/postgres/AlbumsService");
 const AlbumsValidator = require("./validator/albums");
+const SongsService = require("./services/postgres/SongsService");
+const SongsValidator = require("./validator/songs");
 const ClientError = require("./exceptions/ClientError");
 
 const init = async () => {
   const albumsService = new AlbumsService();
+  const songsService = new SongsService();
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -18,13 +22,22 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: albums,
-    options: {
-      service: albumsService,
-      validator: AlbumsValidator,
+  await server.register([
+    {
+      plugin: albums,
+      options: {
+        service: albumsService,
+        validator: AlbumsValidator,
+      },
     },
-  });
+    {
+      plugin: songs,
+      options: {
+        service: songsService,
+        validator: SongsValidator,
+      },
+    },
+  ]);
 
   server.ext("onPreResponse", (request, h) => {
     //mendapatkan konteks response dari request
