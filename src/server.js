b6682d3761c7440c1214f7271189cade2a2a9c90
Riverif <rifkialfiann@gmail.com>
2024-01-24 14:17:34 +0700

    users done

--- src/server.js
+++ src/server.js
@@ -1,17 +1,28 @@
 require("dotenv").config();
 
 const Hapi = require("@hapi/hapi");
+const ClientError = require("./exceptions/ClientError");
+
+//Albums
 const albums = require("./api/albums");
-const songs = require("./api/songs");
 const AlbumsService = require("./services/postgres/AlbumsService");
 const AlbumsValidator = require("./validator/albums");
+
+//Songs
+const songs = require("./api/songs");
 const SongsService = require("./services/postgres/SongsService");
 const SongsValidator = require("./validator/songs");
-const ClientError = require("./exceptions/ClientError");
+
+//Users
+const users = require("./api/users");
+const UsersService = require("./services/postgres/UsersService");
+const UsersValidator = require("./validator/users");
 
 const init = async () => {
   const albumsService = new AlbumsService();
   const songsService = new SongsService();
+  const usersService = new UsersService();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -35,6 +46,13 @@ const init = async () => {
       options: {
         service: songsService,
         validator: SongsValidator,
+      },
+    },
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
       },
     },
   ]);
